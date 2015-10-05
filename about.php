03e14e05ebf0bdeeeb2cfe667327f8c8fb22ee0b
Kyle Simmons <wskyle12@vt.edu>
2015-10-05 00:12:25 -0700

    Changed members jumbotron/style

--- about.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$message = $_POST['message'];
-	$to = 'wskyle12@vt.edu'; 
-	$subject = 'Message from Website';
-	$headers = "From: someone@your-website.com";
-	mail($to, $subject, $body, $headers)
-?>
