8c85a9217f251ad34273a60db962c4e5481d1669
Kyle Simmons <wskyle12@vt.edu>
2015-10-26 17:32:38 -0700

    Moved images around, cleaned up some other stuff

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(function() {
 		$('a[href=#take-the-tour]').click(function() {
 	   $('html, body').animate({
-		scrollTop: $("#index-content-1").offset().top
+		scrollTop: $("#index-content-2").offset().top
 	}, "slide");
 	});
 });
